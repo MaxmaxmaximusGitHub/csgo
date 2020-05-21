97300d3f31bd3fc4d2c695d3d169c28c6cbb3ade
me <MegaMaxmaxmaximus@gmail.com>
2020-05-21 06:34:25 +0300

    save

---
+++ services/server/index.js
@@ -0,0 +1,14 @@
+// import 'core-js'
+// import './catch-exceptions'
+//
+// setInterval(() => {}, 1000)
+//
+// setImmediate(() => {
+//   require('./server')
+// })
+
+const reactive = require('@csgo/reactive')
+
+console.log(reactive)
+
+
